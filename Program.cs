7ce19b05918b00b391e2800f3ea6388da6f76124
defiant00 <defiant00@gmail.com>
2021-12-24 17:05:34 -0800

    initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ChordAnalyzer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Chord Analyzer v0.0.1");
+            Console.WriteLine();
+
+            var keymap = new Keymap();
+            keymap.Grid4x2();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # chordAnalyzer
+
 Keyboard layout analyzer for one-handed layouts.
+
+Based on the corpus by Ian Douglas presented [here](https://zenodo.org/record/5501838/).
